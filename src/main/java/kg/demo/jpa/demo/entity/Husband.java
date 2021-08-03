95706a530e885d1eccf7da644fffb46ade1150ac
Workstation <o0052ib@gmail.com>
2021-08-04 00:50:41 +0600

    @OneToOne(cascade = {DETACH, MERGE, PERSIST, REFRESH})a

--- src/main/java/kg/demo/jpa/demo/entity/Husband.java
+++ src/main/java/kg/demo/jpa/demo/entity/Husband.java
@@ -24,11 +24,10 @@ public class Husband {
     private int age;
 
     @OneToOne(cascade = {DETACH, MERGE, PERSIST, REFRESH})
-    @JoinColumn(name = "wife_id")
     @EqualsAndHashCode.Exclude
     private Wife wife;
 
-    @OneToMany(mappedBy = "wife", cascade = {DETACH, MERGE, PERSIST, REFRESH}, orphanRemoval = true)
+    @OneToMany(mappedBy = "husband", cascade = {DETACH, MERGE, PERSIST, REFRESH}, orphanRemoval = true)
     @EqualsAndHashCode.Exclude
     private Set<Children> children = new HashSet<>();
 
